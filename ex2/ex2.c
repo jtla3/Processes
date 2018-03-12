b851d0a6c5ba658d3156fad233fc8d6e398fc6fb
Mark Marchant <jtla3@yahoo.com>
2018-03-12 13:43:54 -0700

    ex2 done

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -6,9 +6,38 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    // Your code here 
-    
+    // Your code here
+    FILE *fp;
+    int c, pid;
+    fp = fopen("text.txt", "a+");
+    pid = fork();
+
+    while (1)
+    {
+        c = fgetc(fp);
+        if (feof(fp))
+        {
+            break;
+        }
+        printf("%c", c);
+    }
+
+    if (pid == 0)
+    {
+        printf("%d: Child process!\n", getpid());
+        char child_str[] = "This is the child string!\n";
+        fwrite(child_str, 1, sizeof(child_str), fp);
+    }
+    else
+    {
+        printf("%d: Parent process!\n", getpid());
+        char parent_str[] = "This is parent string!\n";
+        fwrite(parent_str, 1, sizeof(parent_str), fp);
+    }
+
+    fclose(fp);
+
     return 0;
 }
