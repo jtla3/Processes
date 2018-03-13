68792a86fb72a40fd8f825f508a1a08280c358c6
Mark Marchant <jtla3@yahoo.com>
2018-03-13 09:31:39 -0700

    ex3 done

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -9,6 +9,19 @@
 int main(int argc, char* argv[])
 {
     // Your code here
+    int ret = fork();
+    int status;
 
+    if(ret < 0) {
+        perror("fork");
+        exit(1);
+    } else if (ret == 0) {
+        printf("Hello!\n");
+    } else {
+        while (waitpid (ret, &status, WNOHANG) == 0) {
+            sleep(1);
+        }
+        printf("Goodbye!\n");
+    }
     return 0;
 }
